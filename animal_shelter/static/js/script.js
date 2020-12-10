78ba536de8c82a3dd2555a62068534bf9cbf9f9c
im83 <programmer38rus@gmail.com>
2020-12-10 17:57:19 +0800

    set map

--- animal_shelter/static/js/script.js
+++ animal_shelter/static/js/script.js
@@ -9,23 +9,23 @@ function init() {
     });
 
     // Строка с адресом, который необходимо геокодировать
-    var address = 'Иркутск, ул. Уритского, 16';
+    // var address = 'Иркутск, ул. Уритского, 16';
 
     // Ищем координаты указанного адреса
     // https://tech.yandex.ru/maps/doc/jsapi/2.1/ref/reference/geocode-docpage/
-    var geocoder = ymaps.geocode(address);
+    // var geocoder = ymaps.geocode(address);
 
     // После того, как поиск вернул результат, вызывается callback-функция
     geocoder.then(
         function (res) {
 
             // координаты объекта
-            var coordinates = res.geoObjects.get(0).geometry.getCoordinates();
+            // var coordinates = res.geoObjects.get(0).geometry.getCoordinates();
 
             // Добавление метки (Placemark) на карту
             var placemark = new ymaps.Placemark(
-                coordinates, {
-                    'hintContent': address,
+                [52.283385, 104.291836], {
+                    'hintContent': 'Ул. Уритского, 16',
                     'balloonContent': 'Время работы: Пн-Пт, с 9 до 20'
                 }, {
                     'preset': 'islands#redDotIcon'
