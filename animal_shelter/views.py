a9f9e2c868c2f611bf34e3c1656eea08893bd908
im83 <programmer38rus@gmail.com>
2020-12-10 18:26:52 +0800

    set map

--- animal_shelter/views.py
+++ animal_shelter/views.py
@@ -51,10 +51,13 @@ class PetsDetailView(DetailView):
 
 class AboutUs(TemplateView):
     template_name = 'about.html'
+    def get_context_data(self, **kwargs):
+        contex = super().get_context_data(**kwargs)
+        context['map'] = True
+        return  contex
 
 class Map(TemplateView):
     template_name = 'map.html'
-
 @csrf_exempt
 def push(request):
     os.system('sudo git pull origin master && sudo systemctl restart gunicorn')
