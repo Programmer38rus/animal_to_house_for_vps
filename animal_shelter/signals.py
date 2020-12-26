872bcd28afae6d2c9f6644bb1e597d802398c51f
im83 <programmer38rus@gmail.com>
2020-12-26 09:34:27 +0800

    up select_related queries in the signals.py

--- animal_shelter/signals.py
+++ animal_shelter/signals.py
@@ -13,19 +13,19 @@ from django.core.cache import cache
 
 @receiver(post_save, sender=Pet)
 def pet_save(sender, instance, **kwargs):
-    cache.set('Pets', sender.objects.all())
+    cache.set('Pets', sender.objects.all().select_related())
 
 
 @receiver(post_delete, sender=Pet)
 def pet_delete(sender, **kwargs):
-    cache.set('Pets', sender.objects.all())
+    cache.set('Pets', sender.objects.all().select_related())
 
 # сигналы для видов животных
 @receiver(post_save, sender=Kind)
 def kind_save(sender, instance, **kwargs):
-    cache.set('kind', sender.objects.all())
+    cache.set('kind', sender.objects.all().select_related())
 
 
 @receiver(post_delete, sender=Kind)
 def kind_delete(sender, **kwargs):
-    cache.set('kind', sender.objects.all())
+    cache.set('kind', sender.objects.all().select_related())
